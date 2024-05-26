9df52739b0e3cee94f4a25bdea15f39d7b970da5
Trần Mạnh Cường <89560029+TranManhCuongQN@users.noreply.github.com>
2024-05-26 10:09:13 +0700

    feat(editImage&notification): add edit image && edit notificaion icon

--- src/features/admin/product/edit-product-tabs/images-tab/ImagesTab.jsx
+++ src/features/admin/product/edit-product-tabs/images-tab/ImagesTab.jsx
@@ -1,16 +1,26 @@
-import React, { useState } from 'react';
-import { Box, Button, Grid, Typography } from '@mui/material';
-import { useSelector } from 'react-redux';
+import React, { useState } from "react";
+import { Box, Button, Grid, Typography } from "@mui/material";
+import { useSelector } from "react-redux";
 
-import ProductImage from './ProductImage';
-import { addImage } from '../../productSlice';
+import ProductImage from "./ProductImage";
+import { addImage } from "../../productSlice";
 
-import { Iconify } from '../../../../../components';
-import ProductImageForm from './ProductImageForm';
+import { Iconify } from "../../../../../components";
+import ProductImageForm from "./ProductImageForm";
 
 const ImagesTab = ({ productId, images }) => {
   const [openProductImageForm, setOpenProductImageForm] = useState(false);
   const { addImageStatus } = useSelector((state) => state.products);
+  const [isEdit, setIsEdit] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  console.log("isEdit", isEdit);
+
+  const handleEditImage = (image) => {
+    setIsEdit(true);
+    setOpenProductImageForm(true);
+    setSelectedImage(image);
+  };
 
   const handleOpenProductImageForm = () => {
     setOpenProductImageForm(true);
@@ -18,32 +28,53 @@ const ImagesTab = ({ productId, images }) => {
 
   const handleCloseProductImageForm = () => {
     setOpenProductImageForm(false);
+    setIsEdit(false);
+    setSelectedImage(null);
   };
 
   return (
     <>
-      <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', mb: 2 }}>
-        <Typography variant='h5' component='h2'>Product images</Typography>
-        <Button variant='contained' color='primary' onClick={handleOpenProductImageForm}>
-          <Iconify icon='material-symbols:add' width={20} height={20} />
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          mb: 2,
+        }}
+      >
+        <Typography variant="h5" component="h2">
+          Product images
+        </Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleOpenProductImageForm}
+        >
+          <Iconify icon="material-symbols:add" width={20} height={20} />
           Add Image
         </Button>
       </Box>
       <Grid container spacing={2}>
         {images.map((image) => (
           <Grid item key={image.id} sm={12} md={2}>
-            <ProductImage image={image.imageUrl} isMain={image.isMain} height='120px' />
+            <ProductImage
+              image={image.imageUrl}
+              isMain={image.isMain}
+              height="120px"
+              onClick={() => handleEditImage(image)}
+            />
           </Grid>
         ))}
       </Grid>
       <ProductImageForm
         open={openProductImageForm}
         handleClose={handleCloseProductImageForm}
-        dialogTitle='Add Product Image'
-        dialogContent='Upload a new image for the product.'
+        dialogTitle={isEdit ? "Edit Product Image" : "Add Product Image"}
+        dialogContent="Upload a new image for the product."
         action={addImage}
         status={addImageStatus}
-        isEdit={false}
+        isEdit={isEdit}
+        image={selectedImage}
         productId={productId}
       />
     </>
