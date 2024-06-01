b5ba323849974bb9e15138a75a7e53d6570453ee
Hoa Vy <vycoi1511@gmail.com>
2024-06-01 18:32:21 +0700

    update env

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,4 +1,4 @@
-export const API_URL = 'https://estore-be.azurewebsites.net';
+export const API_URL = import.meta.env.VITE_API_URL;
 export const NOTIFICATIONS_HUB = 'hubs/notifications';
 // export const API_URL = 'https://hci-be-new.azurewebsites.net';
 // export const API_URL = 'http://localhost:8080';
