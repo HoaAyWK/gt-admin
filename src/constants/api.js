8b523247002b29aee77f4a493b47f0829055144b
Hoa Vy <vycoi1511@gmail.com>
2024-06-01 19:19:06 +0700

    udpate api url

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,4 +1,4 @@
-export const API_URL = import.meta.env.VITE_API_URL || 'https://localhost:7188';
+export const API_URL = import.meta.env.VITE_API_URL || 'https://lionfish-app-fjqdi.ondigitalocean.app';
 export const NOTIFICATIONS_HUB = 'hubs/notifications';
 // export const API_URL = 'https://hci-be-new.azurewebsites.net';
 // export const API_URL = 'http://localhost:8080';
